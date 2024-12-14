d4cb831f4097c73341854d8240b7269ba32d7386
Kwek Zheng Hui <zheng.hui94@gmail.com>
2024-12-14 11:21:13 +0800

    initial commit

---
+++ src/test/java/aquariux/com/cryptotrading/CryptotradingApplicationTests.java
@@ -0,0 +1,13 @@
+package aquariux.com.cryptotrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
